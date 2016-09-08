1dbebcc40ea4422dbb881e7b68e26bc270c58cf4
Amanda Brown <amanda.brown@gmail.com>
2016-09-08 16:31:50 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeDistribution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your age.");
+            int userAge = int.Parse(Console.ReadLine());
+        }
+    }
+}
